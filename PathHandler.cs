f826191e4bb8c5660ff72426f7afbcca8efa9cc9
Uladzislau Charniakou <vlobo2004@gmail.com>
2023-06-15 23:23:19 +0300

    Move path logic to Path Handler
    Signed-off-by: Uladzislau Charniakou <vlobo2004@gmail.com>

---
+++ PathHandler.cs
@@ -0,0 +1,87 @@
+
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+public class PathHandler
+{
+    [Path(@"^\/(css|js).*$")]
+    public async void fun1(HttpContext context)
+    {
+        await context.Response.SendFileAsync(Program.WEB_DIR + context.Request.Path);
+        return;
+    }
+
+    [Path(@"^\/api.*$")]
+    public async void fun2(HttpContext context)
+    {
+        string requestBody;
+        using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8))
+        {
+            requestBody = await reader.ReadToEndAsync();
+        }
+        JsonDocument jd = JsonDocument.Parse(requestBody);
+        int code = Convert.ToInt32(jd.RootElement.GetProperty("code").ToString());
+
+        foreach (var method in typeof(ApiCodeHandler).GetMethods(BindingFlags.Public | BindingFlags.Instance))
+        {
+            var attribute = method.GetCustomAttribute<ApiCodeAttribute>();
+            if (attribute != null && attribute.Code == code)
+            {
+                Task? task = (Task?)(method.Invoke(new ApiCodeHandler(), new Object[] { context, jd }));
+                if (task is not null)
+                {
+                    await task;
+                }
+            }
+        }
+        return;
+    }
+    [Path(@"^\/.*\..*$")]
+    public async void fun3(HttpContext context)
+    {
+        try
+        {
+            await context.Response.SendFileAsync(Program.WEB_DIR + context.Request.Path);
+        }
+        catch (FileNotFoundException)
+        {
+            context.Response.StatusCode = 404;
+            await context.Response.SendFileAsync(Program.WEB_DIR + "/404.html");
+        }
+        return;
+    }
+    [Path(@"\/?.*$")]
+    public async void fun4(HttpContext context)
+    {
+        try
+        {
+            await context.Response.SendFileAsync(Program.WEB_DIR + context.Request.Path + "/index.html");
+        }
+        catch (FileNotFoundException)
+        {
+            context.Response.StatusCode = 404;
+            await context.Response.SendFileAsync(Program.WEB_DIR + "/404.html");
+        }
+        return;
+    }
+
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class PathAttribute : Attribute
+{
+    private string _regex;
+    public PathAttribute(string regex)
+    {
+        this._regex = regex;
+    }
+
+    public string Regex
+    {
+        get
+        {
+            return this._regex;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,66 +19,19 @@ public class Program
         app.Run(async (context) =>
         {
             app.Logger.LogDebug(context.Request.Path);
-            switch (context.Request.Path)
+            foreach (var method in typeof(PathHandler).GetMethods(BindingFlags.Public | BindingFlags.Instance))
             {
-                case var a when new Regex(@"^\/(css|js).*$").IsMatch(a):
+                var attribute = method.GetCustomAttribute<PathAttribute>();
+                if (attribute != null && new Regex(attribute.Regex).IsMatch(context.Request.Path))
+                {
+                    Task? task = (Task?)(method.Invoke(new PathHandler(), new Object[] { context }));
+                    if (task is not null)
                     {
-                        await context.Response.SendFileAsync(WEB_DIR + context.Request.Path);
-                        return;
+                        await task;
                     }
-                case var b when new Regex(@"^\/api.*$").IsMatch(b):
-                    {
-                        string requestBody;
-                        using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8))
-                        {
-                            requestBody = await reader.ReadToEndAsync();
-                        }
-                        JsonDocument jd = JsonDocument.Parse(requestBody);
-                        int code = Convert.ToInt32(jd.RootElement.GetProperty("code").ToString());
-
-                        foreach (var method in typeof(ApiCodeHandler).GetMethods(BindingFlags.Public | BindingFlags.Instance))
-                        {
-                            var attribute = method.GetCustomAttribute<ApiCodeAttribute>();
-                            if (attribute != null && attribute.Code == code)
-                            {
-                                Task? task = (Task?)(method.Invoke(new ApiCodeHandler(), new Object[] { context, jd }));
-                                if (task is not null)
-                                {
-                                    await task;
-                                }
-                            }
-                        }
-
-                        return;
-                    }
-                case var c when new Regex(@"^\/.*\..*$").IsMatch(c):
-                    {
-                        try
-                        {
-                            await context.Response.SendFileAsync(WEB_DIR + context.Request.Path);
-                        }
-                        catch (FileNotFoundException)
-                        {
-                            context.Response.StatusCode = 404;
-                            await context.Response.SendFileAsync(WEB_DIR + "/404.html");
-                        }
-                        return;
-                    }
-                case var d when new Regex(@"\/?.*$").IsMatch(d):
-                    {
-                        try
-                        {
-                            await context.Response.SendFileAsync(WEB_DIR + context.Request.Path + "/index.html");
-                        }
-                        catch (FileNotFoundException)
-                        {
-                            context.Response.StatusCode = 404;
-                            await context.Response.SendFileAsync(WEB_DIR + "/404.html");
-                        }
-                        return;
-                    }
-
+                }
             }
+            return;
         });
         app.Run();
     }
